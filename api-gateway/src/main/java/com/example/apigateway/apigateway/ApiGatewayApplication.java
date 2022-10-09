12d7468b1711c1faf5cff31980fcd4e75ce2ae17
Manjosh R <manjosh.1990@yahoo.com>
2022-10-09 19:13:13 +0530

    added allowed methods to be ignored by CSRF filter

--- api-gateway/src/main/java/com/example/apigateway/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/apigateway/ApiGatewayApplication.java
@@ -3,11 +3,16 @@ package com.example.apigateway.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -15,6 +20,8 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+	private static final Set<HttpMethod> ALLOWED_METHODS = new HashSet<>(
+			Arrays.asList(HttpMethod.GET, HttpMethod.HEAD, HttpMethod.TRACE, HttpMethod.OPTIONS));
 	@Bean
 	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 		http.authorizeExchange().anyExchange().permitAll().and()
@@ -25,7 +32,7 @@ public class ApiGatewayApplication {
 	}
 	private ServerWebExchangeMatcher ignoringFormUrlEncodedContentType() {
 		return (exchange) -> !MediaType.APPLICATION_FORM_URLENCODED.isCompatibleWith(
-				exchange.getRequest().getHeaders().getContentType())
+				exchange.getRequest().getHeaders().getContentType()) || ALLOWED_METHODS.contains(exchange.getRequest().getMethod())
 				? ServerWebExchangeMatcher.MatchResult.match()
 				: ServerWebExchangeMatcher.MatchResult.notMatch();
 	}
